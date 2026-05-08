8131b2617ca7aa52c4cca259b812c39b0d8eaad4
Arman Khilko <i@a.omi.io>
2026-05-08 16:02:29 +0400

    ci(root): align release pipeline with project-scoped tags and pkg-scripts release config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,6 @@
-const typeSubjects = [
-    "build", // Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
-    "chore", // Changes which doesn't change source code or tests e.g. changes to the build process, auxiliary tools, libraries
-    "ci", // Changes to our CI config­uration files and scripts (example scopes: Travis, Circle, Browse­rStack, SauceLabs)
-    "docs", // Everything related to documentation
-    "feat", // The new feature you're adding to a particular application
-    "fix", // A bug fix
-    "perf", // A code change that improves perfor­mance
-    "refactor", // Refactoring a specific section of the codebase
-    "revert", // Reverts a previous commit
-    "style", // Changes that do not affect the meaning of the code (white­-space, format­ting, missing semi-c­olons, etc)
-    "test", // Everything related to testing
-    "release",
-];
+const {
+    TYPE_SUBJECTS,
+} = require("@omi-io/pkg-scripts/release-conventional-commits");
 
 module.exports = {
     "extends": "@commitlint/config-conventional",
@@ -48,6 +37,6 @@ module.exports = {
             },
         ],
         "scope-empty": [2, "never"],
-        "type-enum": [2, "always", typeSubjects],
+        "type-enum": [2, "always", TYPE_SUBJECTS],
     },
 };
